aab120446ea5d3118ef4513ea5a123f3a32ea9c0
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-01-07 19:27:12 +0500

    Add navigation links to dashboard layout and create profile and settings pages

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,9 +1,18 @@
-export default function DashboardLayout({children}){
+import Link from "next/link";
+
+export default function DashboardLayout({ children }) {
     return (
         <section className="min-h-screen flex">
-            <div className="w-1/3 bg-gray-500"></div>
+            <div className="w-1/3 border-r-2 border-purple-800">
+                <Link className="p-2 text-center w-full block hover:bg-purple-300" href='/dashboard/settings'>
+                    Setting
+                </Link>
+                <Link className="p-2 text-center w-full block hover:bg-purple-300" href='/dashboard/profile'>
+                    Profile
+                </Link>
+            </div>
             <div className="w-2/3">{children}</div>
         </section>
     )
-    
+
 }
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    return (
+        <div>
+          Dashboard
+        </div>
+    )
 }
 
 export default Dashboard
