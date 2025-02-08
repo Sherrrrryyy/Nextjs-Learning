59a91737a51f8f5520d5a9c0d85b7ea53c05f828
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-02-08 10:02:49 +0500

    Add Hero component with sliding animations and integrate into Home page

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <>
+        <div className='py-10'>
+            <div className='font-bold ml-24 slide-left text-red-800' style={{fontSize: '100px'}} >BLOG</div>
+            <div className='font-bold mr-36 slide-right text-gray-700' style={{fontSize: '180px'}} >BLOG</div>
+            <div className='font-bold ml-52 slide-left text-purple-800' style={{fontSize: '80px'}} >BLOG</div>
+        </div>
+        </>
+    )
+}
+
+export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-
-
-
+import Hero from "./components/Hero";
 
 
 export default function Home() {
   return (
 
     <div className="flex flex-col items-center min-h-screen p-8 pb-20">
-     
+     <Hero />
     </div>
 
 
